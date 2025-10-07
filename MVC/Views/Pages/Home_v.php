0eaf00ee0766bef45a67a6259c549d1cfe45fb41
vietduc111204 <buiduc1248@gmail.com>
2025-10-07 22:13:46 +0700

    Add files via upload

--- MVC/Views/Pages/Home_v.php
+++ MVC/Views/Pages/Home_v.php
@@ -4,64 +4,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rạp phim</title>
-    <link rel="stylesheet" href="http://localhost/rapphim/Public/CSS/home3.css">
+    <link rel="stylesheet" href="http://localhost/rapphim/Public/CSS/home1.css">
+    <script src="http://localhost/rapphim/Public/JS/home.js"></script>
 </head>
 <body>
 
+<!-- BANNER -->
 <section class="banner">
     <img src="/rapphim/Public/img/10d12d31845bf1eadb4b127d1eb3ca27 (1).png" alt="Khuyến mãi Zalopay">
 </section>
 
+<!-- NỘI DUNG CHÍNH -->
 <main class="main-content">
     <aside class="poster-left">
-        <img src="/rapphim/Public/img/lat-mat-6-1.jpg" alt="Lật Mặt 6: Tấm Vé Định Mệnh">
+        <img src="/rapphim/Public/img/review-phim-lat-mat-8.png" alt="Lật Mặt 8">
     </aside>
 
-    <section class="movie-selection">
+    <section class="featured-movie">
         <h2>Phim Đang Chiếu</h2>
         <div class="slider">
             <button class="prev">&#10094;</button>
             <div class="slider-track">
+                <!-- Các thẻ phim -->
                 <div class="movie-card">
                     <img src="/rapphim/Public/img/522919041_1174611668037785_4808148504690933272_n.jpg" alt="Phim 1">
                     <div class="overlay">
                         <a href="#" class="btn-buy">Mua Vé</a>
                     </div>
+                    <p>Tử chiến trên không</p>
                 </div>
-
                 <div class="movie-card">
                     <img src="/rapphim/Public/img/71DlLjNU0HL.jpg" alt="Phim 2">
                     <div class="overlay">
                         <a href="#" class="btn-buy">Mua Vé</a>
                     </div>
+                    <p>Avatar: The way of water</p>
                 </div>
-
                 <div class="movie-card">
                     <img src="/rapphim/Public/img/486099888_1063647409122859_1252407638629859400_n.jpg" alt="Phim 3">
                     <div class="overlay">
                         <a href="#" class="btn-buy">Mua Vé</a>
                     </div>
+                    <p>Tết ở làng địa ngục</p>
                 </div>
-
                 <div class="movie-card">
                     <img src="/rapphim/Public/img/547857626_813476674548817_6096266279182237597_n.jpg" alt="Phim 4">
                     <div class="overlay">
                         <a href="#" class="btn-buy">Mua Vé</a>
                     </div>
+                    <p>Chị ngã em nâng</p>
                 </div>
-
                 <div class="movie-card">
                     <img src="/rapphim/Public/img/530490398_24542356798691645_2437792584912396890_n.jpg" alt="Phim 5">
                     <div class="overlay">
                         <a href="#" class="btn-buy">Mua Vé</a>
                     </div>
+                    <p>Mưa đỏ</p>
                 </div>
-
                 <div class="movie-card">
                     <img src="/rapphim/Public/img/536279893_122153491598782007_921765304500854029_n.jpg" alt="Phim 6">
                     <div class="overlay">
                         <a href="#" class="btn-buy">Mua Vé</a>
                     </div>
+                    <p>Đồi hành xác</p>
                 </div>
             </div>
             <button class="next">&#10095;</button>
@@ -69,12 +74,54 @@
     </section>
 
     <aside class="poster-right">
-        <img src="/rapphim/Public/img/lat-mat-6-1.jpg" alt="Lật Mặt 6: Tấm Vé Định Mệnh">
+        <img src="/rapphim/Public/img/vn_cjrlr_insta_vert_main_1638x2048_intl.jpg" alt="Lật Mặt 6: Tấm Vé Định Mệnh">
     </aside>
 </main>
 
-<!-- Liên kết file JavaScript riêng -->
-<script src="http://localhost/rapphim/Public/JS/home.js"></script>
+<!-- TIN MỚI & ƯU ĐÃI -->
+<section class="event-wrapper">
+    <aside class="poster-left">
+        <img src="/rapphim/Public/img/full_01.jpg" alt="Poster trái sự kiện">
+    </aside>
+
+   <section class="event-section">
+    <h2>Tin mới & Ưu Đãi</h2>
+    <div class="slider">
+        <button class="prev-event">&#10094;</button>
+        <div class="slider-track-event">
+            <div class="event-card">
+                <img src="/rapphim/Public/img/TDM_.jpg" alt="CGV Member Day">
+                <div class="overlay">
+                    <a href="#" class="btn-buy">Xem Chi Tiết</a>
+                </div>
+            </div>
+            <div class="event-card">
+                <img src="/rapphim/Public/img/350x495_11_.jpg" alt="Happy Birthday">
+                <div class="overlay">
+                    <a href="#" class="btn-buy">Xem chi tiết</a>
+                </div>
+            </div>
+            <div class="event-card">
+                <img src="/rapphim/Public/img/membership_cgv.jpg" alt="Vé nhóm & sự kiện">
+                <div class="overlay">
+                    <a href="#" class="btn-buy">Xem chi tiết</a>
+                </div>
+            </div>
+             <div class="event-card">
+                <img src="/rapphim/Public/img/full_01.jpg" alt="Qùa tặng">
+                <div class="overlay">
+                    <a href="#" class="btn-buy">Xem chi tiết</a>
+                </div>
+            </div>
+        </div>
+        <button class="next-event">&#10095;</button>
+    </div>
+</section>
+
+    <aside class="poster-right">
+        <img src="/rapphim/Public/img/full_01.jpg" alt="Poster phải sự kiện">
+    </aside>
+</section>
 
 </body>
 </html>
