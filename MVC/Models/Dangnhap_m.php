e7ac1e8b5ff5581cf8c6f1428d6c1d1982f33c1c
vietduc111204 <buiduc1248@gmail.com>
2025-10-07 15:30:45 +0700

    Add files via upload

--- MVC/Models/Dangnhap_m.php
+++ MVC/Models/Dangnhap_m.php
@@ -1,24 +1,40 @@
-<?php 
+<?php
 class Dangnhap_m extends connectDB {
 
     public function dangnhapKhachHang($email, $matkhau) {
-        $sql = "SELECT * FROM khachhang WHERE Email='$email' AND matKhau='$matkhau'";
+        $sql = "SELECT * FROM khachhang WHERE Email='$email'";
         $dl = mysqli_query($this->con, $sql);
-        $kq = false;
         if (mysqli_num_rows($dl) > 0) {
-            $kq = true;
+            $row = mysqli_fetch_assoc($dl);
+            if (password_verify($matkhau, $row['matKhau'])) {
+                return true;
+            }
         }
-        return $kq;
+        return false;
     }
 
     public function dangnhapNhanVien($email, $matkhau) {
-        $sql = "SELECT * FROM nhanvien WHERE Email='$email' AND matKhau='$matkhau'";
+        $sql = "SELECT * FROM nhanvien WHERE Email='$email'";
         $dl = mysqli_query($this->con, $sql);
-        $kq = false;
         if (mysqli_num_rows($dl) > 0) {
-            $kq = true;
+            $row = mysqli_fetch_assoc($dl);
+            if (password_verify($matkhau, $row['matKhau'])) {
+                return true;
+            }
         }
-        return $kq;
+        return false;
+    }
+
+    public function dangnhapQuanly($email, $matkhau) {
+        $sql = "SELECT * FROM quanly WHERE Email='$email'";
+        $dl = mysqli_query($this->con, $sql);
+        if (mysqli_num_rows($dl) > 0) {
+            $row = mysqli_fetch_assoc($dl);
+            if (password_verify($matkhau, $row['matKhau'])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 ?>
