5e224666c9a1cf19013e39bff272ec8184409fc5
vietduc111204 <buiduc1248@gmail.com>
2025-10-20 16:15:23 +0700

    Delete Controllers directory

--- Controllers/HomeNhanvien.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class HomeNhanvien extends controller{
-    function Get_data(){
-        $this->view('Masterlayout',[
-            'page'=>'HomeNhanvien_v'
-        ]);
-    }
-}
-?>
--- Controllers/HomeQuanly.php
+++
@@ -1,9 +0,0 @@
-<?php 
-class HomeQuanly extends controller{
-    function Get_data(){
-        $this->view('Masterlayout',[
-            'page'=>'HomeQuanly_v'
-        ]);
-    }
-}
-?>
